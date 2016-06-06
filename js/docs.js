11f3c92e47b152250c67ff9ec1fbab7e1d88aaea
ashleykolodziej <kolodz10@bu.edu>
2016-06-06 14:10:35 -0400

    Updates

--- js/docs.js
+++ js/docs.js
@@ -1,19 +1,25 @@
-jQuery(function($) {
-	/* Side Nav Toggler */
-	$(".navToggle").on("click", function(){
-		$(this).toggleClass("is-open");
-		$("nav").toggleClass("is-open");
-		$("body").toggleClass("nav-open");
-		
-		$(".searchToggle, #quicksearch").removeClass("is-open");
-		
+/**
+ * Toggle behavior for navigation / search buttons.
+ */
+( function ( $ ) {
+	$( '.navToggle' ).on( 'click', function ( e ) {
+		e.preventDefault();
+		$( 'nav, .navToggle' ).toggleClass( 'is-open' );
+		$( '.searchToggle, #quicksearch' ).removeClass( 'is-open' );
+		$( 'body' ).toggleClass( 'nav-open' ).removeClass( 'search-open' );
 	});
-	
-	$(".searchToggle").on("click", function(){
-		$(this).toggleClass("is-open");
-		$("#quicksearch").toggleClass("is-open");
-		
-		$("nav, .navToggle").removeClass("is-open");
-		$("body").removeClass("nav-open");
+
+	$( '.searchToggle' ).on( 'click', function ( e ) {
+		e.preventDefault();
+		$( 'nav, .navToggle' ).removeClass( 'is-open' );
+
+		if( ! $( this ).hasClass( 'is-open' ) ){
+			setTimeout(function(){
+				$( '#q' ).focus();
+			}, 100 );
+		}
+
+		$( '.searchToggle, #quicksearch' ).toggleClass( 'is-open' );
+		$( 'body' ).toggleClass( 'search-open' ).removeClass('nav-open');
 	});
-});
+} ( jQuery ) );
