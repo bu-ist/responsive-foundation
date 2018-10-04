848453e77f79a5d4c843daa58026a7b733978be6
Todd Milliken <milliktr@gmail.com>
2018-10-04 12:16:45 -0400

    Remove refererence to grunt-bower-task
    Allows grunt to run without errors for missing grunt-bower-task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,7 +155,6 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-browser-sync');
-	grunt.loadNpmTasks('grunt-bower-task');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-sass');
