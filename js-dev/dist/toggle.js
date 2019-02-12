308660ab04c8c83bb005d8e61992b79d64818017
Todd Milliken <milliktr@gmail.com>
2019-02-12 10:23:17 -0500

    Change eslint config to use WP eslint plugin
    The WP eslint plugin preset feels more familiar than airbnb, which seems to drastically write every parameter of a function on a new line and seems too much of a departure from the current JS syntax in Foundation.
    
    Also comment out eslint in code climate until https://github.com/codeclimate/codeclimate-eslint/ supports the latest WP eslint plugin.

--- js-dev/dist/toggle.js
+++ js-dev/dist/toggle.js
@@ -1,13 +1,13 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.toggle = toggle;
 
 var _jquery = _interopRequireDefault(require("jquery"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
  * Toggle behavior for navigation / search buttons.
--- js-dev/theme.js
+++ js-dev/theme.js
@@ -2,7 +2,7 @@
  * The main js file for the project.
  *
  * This is the entry point where all modules are imported and compiled together
- * into one resulting `theme.js` or `theme.min.js` file.
+ * into one resulting `theme.js` file.
  *
  * @package ResponsiveFoundation
  */
