a23a2c8a1a8345579cc734ba65639eee05ac057e
ashleykolodziej <kolodz10@bu.edu>
2019-08-23 13:06:56 -0400

    Support to config

--- js/theme.js
+++ js/theme.js
@@ -1,2 +1,111 @@
-!function o(l,i,u){function s(t,e){if(!i[t]){if(!l[t]){var a="function"==typeof require&&require;if(!e&&a)return a(t,!0);if(f)return f(t,!0);var r=new Error("Cannot find module '"+t+"'");throw r.code="MODULE_NOT_FOUND",r}var n=i[t]={exports:{}};l[t][0].call(n.exports,function(e){return s(l[t][1][e]||e)},n,n.exports,o,l,i,u)}return i[t].exports}for(var f="function"==typeof require&&require,e=0;e<u.length;e++)s(u[e]);return s}({1:[function(a,e,r){(function(e){"use strict";var t=a("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(r,"__esModule",{value:!0}),r.toggle=function(){var t=(0,i.default)("body"),a=(0,i.default)(".js-nav-toggle"),r=a.add("nav"),n=(0,i.default)(".js-search-toggle"),o=n.add("#quicksearch");function l(e){r.removeClass("is-open"),!0!==e||(0,i.default)(this).hasClass("is-open")||setTimeout(function(){(0,i.default)("#q").focus()},100),"false"===n.attr("aria-expanded")?n.attr("aria-expanded","true").attr("aria-label","Close search"):n.attr("aria-expanded","false").attr("aria-label","Open search"),o.toggleClass("is-open"),t.toggleClass("search-open").removeClass("nav-open")}n.attr("aria-expanded","false").attr("aria-controls","quicksearch"),a.attr("aria-expanded","false").attr("aria-controls","primary-nav-menu"),a.on("click",function(e){e.preventDefault(),"false"===a.attr("aria-expanded")?a.attr("aria-expanded","true").attr("aria-label","Close menu"):a.attr("aria-expanded","false").attr("aria-label","Open menu"),r.toggleClass("is-open"),o.removeClass("is-open"),t.toggleClass("nav-open").removeClass("search-open")}),n.on({click:function(e){e.preventDefault(),l(!0)},keypress:function(e){13===e.keyCode&&(e.preventDefault(),l(!1))}})};var i=t("undefined"!=typeof window?window.jQuery:void 0!==e?e.jQuery:null)}).call(this,"undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{})},{"@babel/runtime/helpers/interopRequireDefault":3}],2:[function(e,t,a){"use strict";Object.defineProperty(a,"__esModule",{value:!0}),Object.defineProperty(a,"toggle",{enumerable:!0,get:function(){return r.toggle}});var r=e("./modules/toggle");(0,r.toggle)()},{"./modules/toggle":1}],3:[function(e,t,a){t.exports=function(e){return e&&e.__esModule?e:{default:e}}},{}]},{},[2]);
-//# sourceMappingURL=theme.js.map
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function (global){
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.toggle = toggle;
+
+var _jquery = _interopRequireDefault((typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null));
+
+/**
+ * Toggle behavior for navigation / search buttons.
+ *
+ * @package ResponsiveFoundation
+ */
+function toggle() {
+  var $body = (0, _jquery.default)('body');
+  var $toggle = (0, _jquery.default)('.js-nav-toggle');
+  var $toggleitems = $toggle.add('nav');
+  var $searchtoggle = (0, _jquery.default)('.js-search-toggle');
+  var $searchitems = $searchtoggle.add('#quicksearch'); // Add aria attributes for control/expanded if JS is available
+
+  $searchtoggle.attr('aria-expanded', 'false').attr('aria-controls', 'quicksearch');
+  $toggle.attr('aria-expanded', 'false').attr('aria-controls', 'primary-nav-menu');
+  $toggle.on('click', function (e) {
+    e.preventDefault();
+
+    if ($toggle.attr('aria-expanded') === 'false') {
+      $toggle.attr('aria-expanded', 'true').attr('aria-label', 'Close menu');
+    } else {
+      $toggle.attr('aria-expanded', 'false').attr('aria-label', 'Open menu');
+    }
+
+    $toggleitems.toggleClass('is-open');
+    $searchitems.removeClass('is-open');
+    $body.toggleClass('nav-open').removeClass('search-open');
+  });
+
+  function toggleSearchPanel(focus) {
+    $toggleitems.removeClass('is-open');
+
+    if (focus === true && !(0, _jquery.default)(this).hasClass('is-open')) {
+      setTimeout(function () {
+        (0, _jquery.default)('#q').focus();
+      }, 100);
+    }
+
+    if ($searchtoggle.attr('aria-expanded') === 'false') {
+      $searchtoggle.attr('aria-expanded', 'true').attr('aria-label', 'Close search');
+    } else {
+      $searchtoggle.attr('aria-expanded', 'false').attr('aria-label', 'Open search');
+    }
+
+    $searchitems.toggleClass('is-open');
+    $body.toggleClass('search-open').removeClass('nav-open');
+  }
+
+  $searchtoggle.on({
+    click: function click(e) {
+      e.preventDefault();
+      toggleSearchPanel(true);
+    },
+    keypress: function keypress(e) {
+      if (e.keyCode === 13) {
+        e.preventDefault();
+        toggleSearchPanel(false);
+      }
+    }
+  });
+}
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"@babel/runtime/helpers/interopRequireDefault":3}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "toggle", {
+  enumerable: true,
+  get: function get() {
+    return _toggle.toggle;
+  }
+});
+
+var _toggle = require("./modules/toggle");
+
+/**
+ * The main js file for the project.
+ *
+ * This is the entry point where all modules are imported and compiled together
+ * into one resulting `theme.js` file.
+ *
+ * @package ResponsiveFoundation
+ */
+// Import dependencies at the top of the file.
+// Run JS scripts here.
+(0, _toggle.toggle)(); // Export all the components.
+
+},{"./modules/toggle":1}],3:[function(require,module,exports){
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    "default": obj
+  };
+}
+
+module.exports = _interopRequireDefault;
+},{}]},{},[2]);
