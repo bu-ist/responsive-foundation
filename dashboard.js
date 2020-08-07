923137b0b06e7bc42b484fc7a23d8248fcec8929
Ashley Kolodziej <kolodz10@bu.edu>
2020-08-07 10:19:40 -0400

    Move to window onload

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,8 @@
-const tables = document.querySelectorAll( '.table_element' );
-const tablesArray = Array.from( tables );
+window.onload( function () {
+	const tables = document.querySelectorAll( '.table_element' );
+	const tablesArray = Array.from( tables );
 
-tablesArray.map( table => {
-	table.parentElement.parentElement.parentElement.classList.add( 'show' )
-});
+	tablesArray.map( table => {
+		table.parentElement.parentElement.parentElement.classList.add( 'show' )
+	});
+} );
