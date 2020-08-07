baf658098740ea32e5a496f48ace84ac0b8b5f0c
Ashley Kolodziej <kolodz10@bu.edu>
2020-08-07 10:21:53 -0400

    Debugging

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,6 @@
 window.onload( function () {
 	const tables = document.querySelectorAll( '.table_element' );
+	console.log(tables);
 	const tablesArray = Array.from( tables );
 
 	tablesArray.map( table => {
