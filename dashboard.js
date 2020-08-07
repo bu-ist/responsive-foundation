fd036c73ccb7d1f4beefd616daffa3b1f17f75e0
Ashley Kolodziej <kolodz10@bu.edu>
2020-08-07 10:36:46 -0400

    Add classes by title

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,16 @@
 window.onload = function () {
 	const tables = document.querySelectorAll( '.table_element' );
-	console.log(tables);
 	const tablesArray = Array.from( tables );
 
 	tablesArray.map( table => {
 		table.parentElement.parentElement.parentElement.classList.add( 'show' )
 	});
+
+	const titles = document.querySelectorAll( '.section_title' );
+	const titlesArray = Array.from( titles );
+
+	titlesArray.map( title => {
+		let classFromTitle = title.innerText.replace(/[^\w\s]/gi, '').replace(/\s/g, "-").toLowerCase();
+		title.classList.add( classFromTitle );
+	} );
 };
